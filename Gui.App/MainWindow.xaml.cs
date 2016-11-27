a566f12a7efd12b4b80e20e8247fa64d1c17e1e3
Łukasz <luki.lki@gmail.com>
2016-11-27 15:21:43 +0100

    Initial commit

---
+++ Gui.App/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using LibSumo.Net;
+using LibSumo.Net.lib.network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace Gui.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static int DEFAULT_TURN_DEGREE = 25;
+        public static int DEFAULT_SPEED = 50;
+        private static readonly log4net.ILog LOGGER = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        DroneController droneController;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            DroneConnection droneConnection = new WirelessLanDroneConnection("192.168.2.1", 44444, "pc");
+            droneController = new DroneController(droneConnection);
+            
+            droneController.addBatteryListener(b=>LOGGER.Info("BatteryState: " + b));
+            droneController.addCriticalBatteryListener(b=>LOGGER.Info("Critical-BatteryState: " + b));
+            droneController.addPCMDListener(b=>LOGGER.Info("PCMD: " + b));
+            droneController.addOutdoorSpeedListener(b=>LOGGER.Info("Speed: " + b));
+        }
+
+        private void Right_Click(object sender, RoutedEventArgs e)
+        {
+            if (droneController != null)
+            {
+                droneController.right();
+            }
+            else
+            {
+                MessageBox.Show("null");
+            }
+        }
+
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            if (droneController != null)
+            {
+                droneController.forward();
+            }
+            else
+            {
+                MessageBox.Show("null");
+            }
+        }
+
+        private void Tap_Click(object sender, RoutedEventArgs e)
+        {
+            if (droneController != null)
+            {
+                droneController.tap();
+            }
+            else
+            {
+                MessageBox.Show("null");
+            }
+        }
+
+        private void Backward_Click(object sender, RoutedEventArgs e)
+        {
+            if (droneController != null)
+            {
+                droneController.backward();
+            }
+            else
+            {
+                MessageBox.Show("null");
+            }
+        }
+    }
+}
---
+++ LibSumo.Net/command/common/CurrentTime.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+namespace LibSumo.Net.lib.command.common
+{
+
+
+
+
+
+	/// <summary>
+	/// @author  Alexander Bischof
+	/// @author  Tobias Schneider
+	/// </summary>
+	public  class CurrentTime : CommonCommand
+	{
+
+		private static readonly String TIME_FORMATTER = "'T'HHmmsszz";
+		private readonly CommandKey commandKey = CommandKey.commandKey(0, 4, 1);
+		//private readonly Clock clock;
+
+		protected internal CurrentTime()
+		{
+
+			//this.clock = clock;
+		}
+
+		public static CurrentTime currentTime()
+		{
+
+			return new CurrentTime();
+		}
+
+
+		public override byte[] getBytes(int counter)
+		{
+
+			byte[] header = new byte[] {(byte) FrameType.ARNETWORKAL_FRAME_TYPE_DATA_WITH_ACK, ChannelType.JUMPINGSUMO_CONTROLLER_TO_DEVICE_ACK_ID.Id, (byte) counter, 15, 0, 0, 0, commandKey.ProjectId, commandKey.ClazzId, commandKey.CommandId, 0};
+
+			try
+			{
+                    using (MemoryStream outputStream = new MemoryStream())
+					{
+                        byte[] time = new NullTerminatedString(DateTime.Now.ToString(TIME_FORMATTER) + "00").getNullTerminatedString();
+
+                        // Stream encodes as UTF-8 by default; specify other encodings in this constructor
+                        using (var ps = new StreamWriter(outputStream))
+                        {
+					        ps.Write(System.Text.Encoding.UTF8.GetString(header).ToCharArray());
+					        ps.Write(System.Text.Encoding.UTF8.GetString(time).ToCharArray());
+                        }
+					    return outputStream.ToArray();
+					}
+			}
+			catch (Exception e)
+			{
+				throw new CommandException("Could not generate CurrentTime command.", e);
+			}
+		}
+
+
+		new public Acknowledge Acknowledge
+		{
+			get
+			{
+    
+				return Acknowledge.AckAfter;
+			}
+		}
+
+
+		public override string ToString()
+		{
+
+            return "CurrentTime{" + DateTime.Now.ToString(TIME_FORMATTER) + '}';
+		}
+
+
+		public new int waitingTime()
+		{
+
+			return 150;
+		}
+	}
+
+}
